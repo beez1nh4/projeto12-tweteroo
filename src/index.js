5b46feab7cea49d249ea4a36a517b84bc2c68bb7
Melissa <melmihomel@gmail.com>
2022-11-04 16:56:45 -0300

    add routes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+app.use(cors());
+
+app.post("/sign-up", (req, res) => {
+    
+})
+
+app.post("/tweets", (req, res) => {
+    
+})
+
+
+app.get("/tweets", (req, res) => {
+    
+})
+
+
+
+app.listen(5000);
