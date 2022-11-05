681534a8b20a5fe76ff62f8db7f19868ee907e3f
Melissa <melmihomel@gmail.com>
2022-11-05 13:23:43 -0300

    add get signup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import cors from "cors"
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+const users = []
+const tweets = []
+
 
 app.post("/sign-up", (req, res) => {
-    
+    const user = req.body
+    users.push(user)
+    res.send("OK")
 })
 
 app.post("/tweets", (req, res) => {
-    
+    const tweet = req.body
+    tweets.push(tweet)
+    res.send("OK")
 })
 
 
